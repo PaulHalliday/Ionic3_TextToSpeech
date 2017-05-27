0bff2a15445cdb57338a7feb950143760be94262
Paul Halliday <Paul_DEV@pauls-imac.home>
2017-05-27 10:00:43 +0100

    Fixed merging error(s).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,10 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-<<<<<<< HEAD
 import { TextToSpeech } from '@ionic-native/text-to-speech'
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { BrowserModule } from "@angular/platform-browser";
-=======
->>>>>>> 87f9e3241ddcb6ef40cd63ca65315b78083d64b5
 
 @NgModule({
   declarations: [
@@ -16,10 +13,7 @@ import { BrowserModule } from "@angular/platform-browser";
     HomePage
   ],
   imports: [
-<<<<<<< HEAD
     BrowserModule,
-=======
->>>>>>> 87f9e3241ddcb6ef40cd63ca65315b78083d64b5
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -27,15 +21,11 @@ import { BrowserModule } from "@angular/platform-browser";
     MyApp,
     HomePage
   ],
-<<<<<<< HEAD
   providers: [
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     TextToSpeech 
   ]
-=======
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
->>>>>>> 87f9e3241ddcb6ef40cd63ca65315b78083d64b5
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import {TextToSpeech} from '@ionic-native/text-to-speech'; 
 import { NavController } from 'ionic-angular';
-=======
-import { TextToSpeech } from 'ionic-native'
->>>>>>> 87f9e3241ddcb6ef40cd63ca65315b78083d64b5
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-<<<<<<< HEAD
 
   text: string;
 
@@ -26,18 +21,6 @@ export class HomePage {
     catch(e){
       console.log(e);
     }
-=======
-  text: string = "";
-  constructor() {
-
-  }
-
-  read(): void {
-    //Read the text from the model via TTS
-    TextToSpeech.speak(this.text)
-      .then(() => console.log('Success'))
-      .catch((reason: any) => console.log(reason));
->>>>>>> 87f9e3241ddcb6ef40cd63ca65315b78083d64b5
   }
 
 }
