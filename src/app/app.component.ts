55767d2d4796d8334af1a6e44f481974ddf6d48b
Paul Halliday <Paul_DEV@pauls-imac.home>
2017-05-27 09:57:28 +0100

    Ionic 3.x.>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-<<<<<<< HEAD
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-=======
-import { StatusBar, Splashscreen } from 'ionic-native';
->>>>>>> 87f9e3241ddcb6ef40cd63ca65315b78083d64b5
 
 import { HomePage } from '../pages/home/home';
 
@@ -16,21 +12,12 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage = HomePage;
 
-<<<<<<< HEAD
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-=======
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-      Splashscreen.hide();
->>>>>>> 87f9e3241ddcb6ef40cd63ca65315b78083d64b5
     });
   }
 }
